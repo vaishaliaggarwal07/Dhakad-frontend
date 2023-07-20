e30a902df27077d3f388b60173a67feaf016c8f7
Anurooop S <anuroop@appzui.com>
2023-07-20 17:16:06 +0530

    OTP input screen done

--- src/Components/Helper/Modal/OTPLogin.js
+++ src/Components/Helper/Modal/OTPLogin.js
@@ -14,7 +14,7 @@ import PhotoCamera from '@material-ui/icons/PhotoCamera';
 const OTPLogin = (props)=>{
 
     const [anchorEl, setAnchorEl] = useState(null);
-    const [isOTPSent, setIsOTPSent] = useState(false);
+    const [isOTPSent, setIsOTPSent] = useState(true);
     const handleClick = (event) => {
         setAnchorEl(event.currentTarget);
     };
@@ -25,6 +25,41 @@ const OTPLogin = (props)=>{
 
     const handleSendOTP = ()=>{
         setIsOTPSent(true);
+    }
+
+    const handlePhoneNumberSubmit = (values,setSubmitting)=>{
+        console.log('OTPLogin:handlePhoneNumberSubmit: ',values,setSubmitting);
+        setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+        }, 400);
+    }
+
+    const handleOTPSubmit = (values,setSubmitting)=>{
+        console.log('OTPLogin:handleOTPSubmit: values ',values);
+    }
+    const handleKeyDownForOtpInput = (event)=>{
+        if(event.key==='-'){
+            event.preventDefault();
+        }
+        if(event.code !=='Backspace'){
+            if(event.target.value && event.target.value.length === 1){
+                event.preventDefault()
+            }
+        }
+    }
+
+    const handleKeyUpForOTPInput = (event)=>{
+        if(!isNaN(+event.key) && event.target.value!=='' && event.target.nextElementSibling){
+            event.target.nextElementSibling.focus();
+        }
+        if(event.code ==='Backspace' && event.target.previousElementSibling){
+            event.target.previousElementSibling.focus();
+        }
+    }
+
+    const verifyOTP = (event)=>{
+
     }
 
     return (
@@ -42,12 +77,7 @@ const OTPLogin = (props)=>{
                     }*/
                     return errors;
                 }}
-                onSubmit={(values, { setSubmitting }) => {
-                    setTimeout(() => {
-                        alert(JSON.stringify(values, null, 2));
-                        setSubmitting(false);
-                    }, 400);
-                }}>
+                onSubmit={handlePhoneNumberSubmit}>
 
 
                 {({ isSubmitting }) => (
@@ -76,12 +106,10 @@ const OTPLogin = (props)=>{
                         <div className="flex flex-column">
                             <div className={classes.mobileInputBox}>
                                 <div className={classes.countryCodePlaceholder}>+91</div>
-                                <Field className={classes.mobileInput} type="number" name="mobile" placeholder="Enter mobile number" />
+                                <Field className={classes.numberInput} type="number" name="mobile" placeholder="Enter mobile number" />
                             </div>
                             <ErrorMessage className={classes.errorText} name="mobile" component="div" />
                         </div>
-
-
 
                         <Button variant="contained" className={classes.otpBtn} disableElevation onClick={handleSendOTP}>
                             GET OTP
@@ -90,6 +118,36 @@ const OTPLogin = (props)=>{
                 )}
             </Formik>
             }
+
+            {isOTPSent && <Formik
+                initialValues={{OTPNumber1:'',OTPNumber2:'',OTPNumber3:'',OTPNumber4:''}} onSubmit={handleOTPSubmit}>
+
+                {({ isSubmitting }) => (
+                    <Form className={`flex flex-column ${classes.otpInputSection} pt-5 pb-5`}>
+                        <span className={classes.OTPEnterText}>Please enter OTP to verify</span>
+                        <span className={classes.OTPSentText}>OTP has sent on mobile number +91 xxxxxxxxxxx</span>
+
+                        <div className={`flex flex-row justify-content-center mt-4 ${classes.otpInputContainer}`}>
+                            <Field className={`${classes.numberInput} ${classes.otpInput}`} type="number" name="OTPNumber1" min="0" max="9" onKeyDown={handleKeyDownForOtpInput} onKeyUp={handleKeyUpForOTPInput} />
+                            <Field className={`${classes.numberInput} ${classes.otpInput}`} type="number" name="OTPNumber2" min="0" max="9" onKeyDown={handleKeyDownForOtpInput} onKeyUp={handleKeyUpForOTPInput} />
+                            <Field className={`${classes.numberInput} ${classes.otpInput}`} type="number" name="OTPNumber3" min="0" max="9" onKeyDown={handleKeyDownForOtpInput} onKeyUp={handleKeyUpForOTPInput} />
+                            <Field className={`${classes.numberInput} ${classes.otpInput}`} type="number" name="OTPNumber4" min="0" max="9" onKeyDown={handleKeyDownForOtpInput} onKeyUp={handleKeyUpForOTPInput} />
+                        </div>
+
+                        <span className={classes.timeRemainingText}>1:00</span>
+
+                        <div className="w-100 flex flex-row justify-content-center mt-4">
+                            <Button variant="contained" className={classes.verifyOTPBtn} disableElevation onClick={verifyOTP}>
+                                Verify OTP
+                            </Button>
+                        </div>
+
+                    </Form>
+                )}
+            </Formik>
+            }
+
+
         </>
     );
 }
