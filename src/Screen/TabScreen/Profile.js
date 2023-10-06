502e76e70c694f88943d20a3b9b95ff4b3808f7a
vaishaliaggarwal07 <65409798+vaishaliaggarwal07@users.noreply.github.com>
2023-10-06 11:14:32 +0530

    Update Profile.js

--- src/Screen/TabScreen/Profile.js
+++ src/Screen/TabScreen/Profile.js
@@ -26,7 +26,7 @@ const PersonalDetils = () => {
     firstName: Yup.string().matches(/^[a-zA-Z\s]*$/,'Only letters and spaces are allowed for First Name').required("First Name is required"),
     lastName: Yup.string().matches(/^[a-zA-Z\s]*$/, 'Only letters and spaces are allowed for Last Name').required("Last Name is required"),
     email: Yup.string().email("Invalid email address").required("Email is required"),
-    mobile: Yup.string().matches(/^\+91[0-9]{10}$/, 'Invalid phone number').required("Phone number is required"),
+    mobile: Yup.string().matches(/^\+91[0-9]{10}$/, 'Invalid phone number (Enter country code if not included)').required("Phone number is required"),
     dateOfBirth: Yup.date().max(new Date(),"Date of Birth cannot be in future").test("year-length","Year must ne 4 digits",function (value){
       if (!value) return true;
       const year = value.getFullYear();
